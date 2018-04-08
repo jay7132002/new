8a523b5bc644b5ce4269d4cd45ffcda65072d333
jay7132002 <jay7132002@gmail.com>
2018-04-08 14:17:05 +0800

    第一次練習

---
+++ ~AutoRecover.Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a= 0.1;
+            decimal b = 0.1;
+            decimal c = 0.1 d;
+            Console.WriteLine(a+b ==2*c);
+            Console.WriteLine(1/2);
+
+        }
+    }
+}
